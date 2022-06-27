34d9e12d55f53926bb88b2bc5d1fea4ca22ff727
Kojo Kumi <kojokumi16.kk@gmail.com>
2022-06-27 22:51:46 +0000

    commit 7

--- embody.js
+++ embody.js
@@ -1,1 +1,35 @@
-let Days = 
+
+let monday = document.getElementById('monday');
+
+console.log(monday.nodeType);
+
+monday.onmouseenter = function(){
+    // monday.style.backgroundColor = 'blue';
+}
+
+// grabbing inner_self
+
+const flasher = document.createElement('span');
+const flasher_pikin = document.createTextNode("$500");
+
+flasher.appendChild(flasher_pikin);
+
+document.getElementsByClassName('.inner_self').appendChild(flasher);
+
+
+// ### FIRTS TRY ##### 
+
+// for( var i = 0; i < bars.length; i++){
+//     bars[i].addEventListener('mouseover', hoverEffects );
+//   };
+
+// function hoverEffects(e){
+     
+//     // for( var i = 0; i < bars.length; i++){
+//     //     bars[i].style.backgroundColor = 'green';
+//     //   };
+     
+//     let mon = document.getElementById('monday')
+      
+// };
+
